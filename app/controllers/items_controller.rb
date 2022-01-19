2aa1a57934c7d7b636d9da008ceb2cc7a8c85918
go_nakao <nakaog5@gmail.com>
2022-01-19 17:07:58 +0900

    削除機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,9 +19,17 @@ class ItemsController < ApplicationController
     redirect_to items_url, notice: "貸出品「#{item.item_name}」を登録しました。"
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    # binding.pry
+    item.destroy
+    redirect_to items_url, notice: "貸出品「#{item.item_name}」を削除しました。"
+  end
+
   private
 
   def item_params
+    # binding.pry
     params.require(:item).permit(:item_name)
   end
 end
