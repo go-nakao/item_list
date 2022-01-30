5c5a33948782ab6137754171ddf4ac5b3b08a4de
go_nakao <nakaog5@gmail.com>
2022-01-30 22:59:44 +0900

    user モデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_144737) do
+ActiveRecord::Schema.define(version: 2022_01_30_134646) do
 
   create_table "items", force: :cascade do |t|
     t.string "item_name", null: false
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2022_01_26_144737) do
     t.date "loan_date"
     t.date "return_date"
     t.text "remark"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
