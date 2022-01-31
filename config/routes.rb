11d3ef1be77e4e750444873e9cbe22157b0bbe02
go_nakao <nakaog5@gmail.com>
2022-01-31 23:19:16 +0900

    bin/rails g controller Admin::Users new edit show index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
+
   root to: 'items#index'
   resources :items, only: [:index, :new, :create, :destroy, :edit, :show, :update]
 end
