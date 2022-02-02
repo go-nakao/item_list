616d02342c6506864bca75e280620b46bbf4a6c0
go_nakao <nakaog5@gmail.com>
2022-02-02 19:41:21 +0900

    ユーザー一覧表示

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,16 @@
 class Admin::UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save #登録後、ユーザー一覧画面に遷移
+      redirect_to admin_users_url(@user), notice: "ユーザー「#{ @user.name }」を登録しました。"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -9,5 +20,13 @@ class Admin::UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmaion)
+  end
+
 end
