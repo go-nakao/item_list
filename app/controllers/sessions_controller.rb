6954598852829f0b19757196ad1817b87a1f1603
go_nakao <nakaog5@gmail.com>
2022-02-07 22:13:38 +0900

    ログイン

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,26 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: session_params[:email])
+
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'ログインしました'
+    else
+      redirect_to login_path, notice: 'ログインに失敗しました'
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
+  #ログイン・ログアウト関連
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   namespace :admin do
     resources :users
