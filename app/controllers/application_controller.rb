2fc2fc4a7c5181befce76ccc50ec389bc720940d
go_nakao <nakaog5@gmail.com>
2022-02-09 20:15:33 +0900

    ログアウト機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
+
+# nilガード 左辺がnilの場合は値が代入され、既に値が入っている場合は値の代入は行われません。
